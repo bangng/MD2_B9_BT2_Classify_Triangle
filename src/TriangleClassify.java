f20e90179ab26ed30de2bf21a61598257b65fc31
bangng <montersb@gmail.com>
2022-08-04 09:17:38 +0700

    ok

--- src/TriangleClassify.java
+++ src/TriangleClassify.java
@@ -1,23 +1,23 @@
 public class TriangleClassify {
     public static String classifyTriangle(int a,int b, int c){
         boolean isTriangle = (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a);
-        boolean isCanTriangle = (a == b || b == c || c == a);
-        boolean isDeuTriangle = (a == b && b == c);
+        boolean isIsoscelesTriangle = (a == b || b == c || c == a);
+        boolean isEquilateralTriangle = (a == b && b == c);
         boolean isSquareTriangle = (a * a + b * b ==c * c )|| (b * b + c * c ==a * a) || (a * a + c * c == b* b);
-        boolean isSquareCanTriangle = isSquareTriangle && isCanTriangle;
+        boolean isSquareIsoscelesTriangle = isSquareTriangle && isIsoscelesTriangle;
 
         if (isTriangle){
-            if (isCanTriangle){
-                return "Là tam giác Cân: ";
+            if (isEquilateralTriangle){
+                return "Là tam giác đều";
             }else {
-                if (isDeuTriangle){
-                    return "Là tam giác Đều: ";
+                if (isIsoscelesTriangle){
+                    return "Là tam giác cân";
                 }else if (isSquareTriangle){
-                    return "Là tam giác vuông: ";
-                }else if (isSquareCanTriangle){
-                    return "Là tam giác Vuông Cân: ";
+                    return "Là tam giác vuông";
+                }else if (isSquareIsoscelesTriangle){
+                    return "Là tam giác vuông cân";
                 }else {
-                    return "Tam giác Thường: ";
+                    return "Là tam giác thường";
                 }
             }
         }else {
--- src/TriangleClassifyTest.java
+++ src/TriangleClassifyTest.java
@@ -8,10 +8,72 @@ class TriangleClassifyTest {
         int a = 2;
         int b = 2;
         int c = 2;
-        String expected = "Là tam giác Đều";
+        String expected = "Là tam giác đều";
         String actual = TriangleClassify.classifyTriangle(a, b, c);
         assertEquals(expected, actual);
     }
+
+    @Test
+    void testTriangle22(){
+        int a = 2;
+        int b = 2;
+        int c = 3;
+        String expected = "Là tam giác cân";
+        String actual = TriangleClassify.classifyTriangle(a, b, c);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testTriangle345(){
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        String expected = "Là tam giác vuông";
+        String actual = TriangleClassify.classifyTriangle(a, b, c);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testTriangle823(){
+        int a = 8;
+        int b = 2;
+        int c = 3;
+        String expected = "Not a Triangle";
+        String actual = TriangleClassify.classifyTriangle(a, b, c);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testTriangle_121(){
+        int a = -1;
+        int b = 2;
+        int c = 1;
+        String expected = "Not a Triangle";
+        String actual = TriangleClassify.classifyTriangle(a, b, c);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testTriangle011(){
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        String expected = "Not a Triangle";
+        String actual = TriangleClassify.classifyTriangle(a, b, c);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testTriangle456(){
+        int a = 4;
+        int b = 5;
+        int c = 6;
+        String expected = "Là tam giác thường";
+        String actual = TriangleClassify.classifyTriangle(a, b, c);
+        assertEquals(expected, actual);
+    }
+
+
 
 
 }
